e2dc7cf4783c10475de319dc621205813107e80c
Kobe Wilson <kobe.wilson81@gmail.com>
2019-01-07 21:21:01 -0500

    Initial commit

---
+++ weather/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+import requests
+from .models import City
+from .forms import CityForm
+
+
+def index(request):
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=18d5c38e566ef19504b59bbaa6f06ae0'
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city' : city.name,
+            'temperature' : r['main']['temp'],
+            'description' : r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
+
+    context = {'weather_data' : weather_data, 'form' : form}
+    return render(request, 'weather/weather.html', context)
